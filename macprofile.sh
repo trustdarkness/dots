700fddf4f01d5cbf70b107f1b53eef14927c219b
__mt__ <mt@trustdarkness.com>
2024-12-17 01:51:57 -0600

    fix runonce paradigm, no .ds_store on network volumes; powerline setup

--- macprofile.sh
+++ macprofile.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+launchctl setenv MACPROFILED TRUE
+
+launchctl setenv POWERLINE TRUE
 
 # these get rid of the annoying bubbles and update nags in systemprefs if you are
 # version locked because you do real work with your computer like some of us
@@ -6,20 +9,22 @@ alias killupdates='defaults write com.apple.systempreferences DidShowPrefBundleI
 alias killbubbles="defaults write com.apple.systempreferences AttentionPrefBundleIDs 0 && killall Dock"
 
 # sometimes these defaults reads can be non-instant
-bubbleskilled=$(launchctl getenv bubbleskilled)
-if [ -z "$bubbleskilled" ]; then
-  se "checking for update and icloud warnings to clear..."
+bubblecheck=$(launchctl getenv bubblecheck)
+if [ -z "$bubblecheck" ]; then
+  launchctl setenv bubbleskilled TRUE
+  se "[runonce / session] checking for update and icloud warnings to clear..."
   if gt "$(defaults read com.apple.systempreferences AttentionPrefBundleIDs)" 0; then
     killupdates
     killbubbles
-    launchctl setenv bubbleskilled true
   fi
 fi
 
-relaunch=$(defaults read com.apple.loginwindow LoginwindowLaunchesRelaunchApps)
-if [ -n "$relaunch" ] && [ "$relaunch" -ne 0 ]; then
-  defaults write com.apple.loginwindow LoginwindowLaunchesRelaunchApps -bool false
+ds_store_network=$(defaults read com.apple.desktopservices DSDontWriteNetworkStores)
+if [ -z "$ds_store_network" ] || [ "$ds_store_network" -ne 1 ]; then 
+  defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
 fi
-  
-launchctl setenv MACPROFILED true
+relaunch=$(defaults read com.apple.loginwindow LoginwindowLaunchesRelaunchApps)
+if [ -z "$relaunch" ] || [ "$relaunch" -ne 0 ]; then
+  defaults write com.apple.loginwindow LoginwindowLaunchesRelaunchApps -bool FALSE
+fi
   
