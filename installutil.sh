087b6aecac04ced945eed78f1ae39aefab83f515
Mike Thompson <mt@trustdarkness.com>
2025-01-17 19:03:24 -0600

    add a yay mode with noconfirm

--- installutil.sh
+++ installutil.sh
@@ -11,7 +11,7 @@ elif [[ $(uname) == "Darwin" ]]; then
   function sai() {
     brew install $@
   }
-  # brew already doesn't ask for confirmation, but for 
+  # brew already doesn't ask for confirmation, but for
   # cross platform consistency
   function sayi() {
     sai $@
@@ -29,7 +29,7 @@ elif [[ $(uname) == "Darwin" ]]; then
     sudo softwareupdate --list
   }
   function salu_macos() {
-    if mist=$(type -p mist); then 
+    if mist=$(type -p mist); then
       $mist list installer
     else
       sudo softwareupdate --list-full-installers
@@ -46,11 +46,25 @@ if string_contains "(arch|Manjaro|endeavour)" "$distro"; then
   function sayi() {
     sudo pacman -Sy --noconfirm $@
   }
-  function yayi() {
+  function yai() {
     yay -Sy $@
   }
+  function yayi() {
+    local localbin="$HOME/.local/bin/"
+    local yp="$localbin/ypacman"
+    if ! [ -x "$HOME/.local/bin/ypacman" ]; then
+      if ! [ -f "$yp" ]; then
+        se "no ypacman in $HOME/.local/bin"
+        if confirm_yes "create this?"; then
+          printf '#!%s %s\n%s --noconfirm $@\n' "$(type -p env)" "bash" "$(type -p pacman)" > "$yp"
+          chmod +x "$yp"
+        fi
+      fi
+    fi
+    yay --pacman "$yp" -Sy $@
+  }
   function sau() {
-    sudo pacman -Syu 
+    sudo pacman -Syu
   }
   function sauu() {
     sudo pacman -Syu $@
@@ -59,7 +73,7 @@ if string_contains "(arch|Manjaro|endeavour)" "$distro"; then
     sudo pacman -Ss $@
   }
   function sasn() {
-    sudo pacman -Ss "^${1:-}" 
+    sudo pacman -Ss "^${1:-}"
    }
   function sasy() {
     yay -Ss $@
@@ -90,7 +104,7 @@ if string_contains "(fedora|nobara)" "$distro"; then
     sudo dnf install -y $@
   }
   function sau() {
-    sudo dnf upgrade -y 
+    sudo dnf upgrade -y
   }
   function sauu() {
     sudo dnf upgrade -y $@
@@ -117,7 +131,7 @@ if string_contains "(Debian|Ubuntu)" "$distro"; then
     sudo apt install -y $@
   }
   function sau() {
-    sudo aptitude update 
+    sudo aptitude update
   }
   function sauu() {
     sudo aptitude update && sudo aptitude upgrade
@@ -148,14 +162,14 @@ if string_contains "(Debian|Ubuntu)" "$distro"; then
     sudo apt-cache search $@
   }
   function safs() {
-    if ! type -p apt-file 2>&1 > /dev/null; then 
+    if ! type -p apt-file 2>&1 > /dev/null; then
       sayi apt-file
       sudo apt-file update
     fi
     sudo apt-file find
   }
   function vasl() {
-    sudo vim /etc/apt/sources.list 
+    sudo vim /etc/apt/sources.list
   }
   function asl() {
     echo /etc/apt/sources.list
@@ -251,7 +265,7 @@ if string_contains "(Debian|Ubuntu)" "$distro"; then
     sudo sed -i.bak 's@deb\ https://deb.debian.org/debian/\ oldstable@#deb\ https://deb.debian.org/debian/\ oldstable@g' /etc/apt/sources.list
     sau
   }
-  
+
   function sai-unstable {
     pattern=$1
     sudo sed -E -i.bak 's@#deb(.*)sid@deb\1sid@g' /etc/apt/sources.list
